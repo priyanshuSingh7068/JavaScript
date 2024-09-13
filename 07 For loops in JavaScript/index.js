461cd57518878ae98e26c56222f0c3c4f9bab4eb
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-13 22:07:11 +0530

    for loops

---
+++ 07 For loops in JavaScript/index.js
@@ -0,0 +1,32 @@
+// For loops in JavaScript *****
+
+// For loop *****
+
+// let sum = 0;
+// let n = prompt("Enter the value of n");
+// n = Number.parseInt(n);
+// for (let i = 0; i < n; i++) {
+//   sum += i + 1;
+//   // console.log((i+1), "+");
+// }
+// console.log("Sum of first " + n + " natural number is " + sum);
+
+// For in loop *****
+
+// let obj = {
+//   priyanshu: 90,
+//   shubh: 45,
+//   harry: 56,
+//   shivika: 58,
+//   shiv: 34,
+// };
+// for (let a in obj) {
+// //   console.log("Marks of " + a + " are " + obj[a]);
+//   console.log(`Marks of ${a} are ${obj[a]}`);
+// }
+
+// For of loop *****
+
+for (let a of "priyanshu") {
+  console.log(a);
+}
