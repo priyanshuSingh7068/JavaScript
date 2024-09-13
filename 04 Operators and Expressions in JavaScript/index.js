18a34bdc46ca8a8e4ecab792fe6ab81319b9ba17
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-13 21:30:23 +0530

    operators

---
+++ 04 Operators and Expressions in JavaScript/index.js
@@ -0,0 +1,45 @@
+// Operators and Expressions in Js *****
+
+// 77;
+// ("Priyanshu");
+// false;
+
+// Arithmetic Operators *****
+
+console.log("Operators in Js");
+let a = 10;
+let b = 4;
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a / b =", a / b);
+console.log("a * b =", a * b);
+console.log("a ** b =", a ** b);
+console.log("a % b =", a % b);
+console.log("++a =", ++a);
+console.log("a++ =", a++);
+console.log("--a =", --a);
+console.log("a-- =", a--);
+
+// Assignment Operators *****
+
+let assignment = 5;
+assignment += 5; // same as assignment = assignment + 5
+console.log(assignment);
+
+// Comparison Operators *****
+
+let comparison1 = 6;
+let comparison2 = 7;
+console.log("comparison1 == comparison2 is ", comparison1 == comparison2);
+console.log("comparison1 != comparison2 is ", comparison1 != comparison2);
+console.log("comparison1 === comparison2 is ", comparison1 === comparison2);
+console.log("comparison1 !== comparison2 is ", comparison1 !== comparison2);
+console.log("comparison1 > comparison2 is ", comparison1 > comparison2);
+
+// Logical Operators *****
+
+let x = 5
+let y = 6
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!true);
