11f2a67f139e7515ee69b9673411530e27bb9f3c
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-13 21:20:03 +0530

    variables

---
+++ 01 Variables in JavaScript/index.js
@@ -0,0 +1,15 @@
+// Variables *****
+
+// var a = 45;
+// var a = "p";
+let b = "Priyanshu";
+const author = "Priyanshu";
+// author = 5; // Throws an error because constant cannot be changed
+b = 4;
+let c = null;
+let d = undefined;
+{
+  let b = "this";
+  console.log(b);
+}
+console.log(b);
