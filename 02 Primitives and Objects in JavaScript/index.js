df42267350c4ba178117ae1612ab1f37417f54db
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-13 21:25:16 +0530

    primitives and objects

---
+++ 02 Primitives and Objects in JavaScript/index.js
@@ -0,0 +1,31 @@
+// Primitives and Objects in Js
+
+// Primitives in Js *****
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("5445");
+let e = "Priyanshu";
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+console.log(a, b, c, d, e, f, g);
+console.log(
+  typeof a,
+  typeof b,
+  typeof c,
+  typeof d,
+  typeof e,
+  typeof f,
+  typeof g
+);
+
+// Objects in Js *****
+
+const item = {
+  name: "Priyanshu singh",
+  course: "JS",
+  city: "Kanpur",
+};
+console.log(item);
+console.log(item["name"]);
+console.log(item["code"]); // undefined
