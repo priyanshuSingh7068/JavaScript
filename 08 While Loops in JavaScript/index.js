f67f7e291990c494a6fe4ceb74bfd7163819b998
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-13 22:23:59 +0530

    while loops

---
+++ 08 While Loops in JavaScript/index.js
@@ -0,0 +1,23 @@
+// While Loops in JavaScript *****
+
+// While loop *****
+
+// let n = prompt("Enter the value of n");
+// n = Number.parseInt(n);
+// let i = 0;
+// while (i <= n) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do while loop *****
+
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+let i = 0;
+do {
+  while (i <= n) {
+    console.log(i);
+    i++;
+  }
+} while (i <= n);
