99b0ebbaabd4c0840ab414c015540fc5439fc5f7
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-14 17:35:34 +0530

    array methods

---
+++ 16 Some More JavaScript Array Methods/index.js
@@ -0,0 +1,27 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr1 = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+// console.log(arr.length);
+// delete arr[0];
+// console.log(arr.length);
+
+// let newarr = arr.concat(arr1);
+// console.log(newarr);
+
+// sort method *****
+
+// let compare = (a, b) => {
+//   return a - b;
+// };
+// let array = [34, 56, 12, 3, 1, 66, 34, 2, 99];
+// array.sort(compare);
+// array.reverse();
+// console.log(array);
+
+// splice and slice *****
+
+let array = [34, 56, 12, 3, 1, 66, 34, 2, 99];
+// array.splice(2, 3, 1021, 1022, 1023);
+// console.log(array);
+
+let newarray = array.slice(3, 5);
+console.log(newarray);
