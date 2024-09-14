0f4b9cdf1107cd8c728a5e06c1073a5893f496b6
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-14 17:37:11 +0530

    loops with array

---
+++ 17 Using Loops With Arrays/index.js
@@ -0,0 +1,34 @@
+// Using Loops With Arrays in JavaScript *****
+
+let arr = [3, 54, 1, 2, 4];
+
+// for loop *****
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for each *****
+
+// arr.forEach((element) => {
+//   console.log(element * element);
+// });
+
+// Array.from *****
+
+let name = "Priyanshu";
+let carr = Array.from(name);
+console.log(carr);
+
+// for...of *****
+
+for (let i of arr) {
+  console.log(i);
+}
+
+// for...in *****
+
+for(let i in arr){
+    console.log(arr[i]);
+    
+}
