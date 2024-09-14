d4d30175a637e5283d87bc6fc091330b1a49cfb1
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-14 13:35:44 +0530

    string methods

---
+++ 12 String Methods/index.js
@@ -0,0 +1,18 @@
+//  String Methods in JS *****
+
+let name = "Priyanshu";
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(2, 4));
+// console.log(name.slice(2));
+// console.log(name.replace("iy", "yi"));
+let friend = "Naman";
+console.log(name.concat(` is a friend of ${friend} Ok`));
+let friend1 = "            meena        ";
+console.log(friend1.trim());
+let fr = "Shivika"
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
