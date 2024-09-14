2eab61f5c693f8a9d8dd7d2e89ea862a3ce09207
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-14 11:07:05 +0530

    introduction to string

---
+++ 11 Introduction to Strings/index.js
@@ -0,0 +1,23 @@
+// Introduction to Strings *****
+
+let myname = "Priyanshu";
+// console.log(myname);
+// console.log(myname.length);
+// console.log(myname[0]);
+// console.log(myname[1]);
+
+let friend = "Adarsh";
+// console.log(friend);
+
+// Template literals *****
+
+let boy1 = "Promod";
+let boy2 = "Nikhil";
+console.log(`${boy2} is a friend of ${boy1}`);
+
+// Escape Sequence Characters *****
+
+// let fruit = 'Bana\'na';
+// console.log(fruit);
+let fruit = 'Bana"na';
+console.log(fruit);
