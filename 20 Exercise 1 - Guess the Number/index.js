39ef8d3079d87f50b110cae5b00e5b954de30347
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-29 10:17:12 +0530

    exercise

---
+++ 20 Exercise 1 - Guess the Number/index.js
@@ -0,0 +1,10 @@
+// Exercise 1 - Guess the Number
+
+//  Write a JavaScript program to generate a random number and store it in a variables. The program them takes an input from the user to tell them whether the guess was correct ,greater or less than the original number.
+
+
+
+// Keep asking for the number untill the correct number is guessed.
+// Increment the cha nces variable.
+
+// Evantually after the game is over, display the (100 - Number of chances) and the actual number.
