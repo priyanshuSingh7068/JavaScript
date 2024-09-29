1c04bea297b18ee15077ed4cb365dd04aa590b44
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-29 09:51:52 +0530

    operators and expressions

--- 04 Operators and Expressions in JavaScript/index.js
+++ 04 Operators and Expressions in JavaScript/index.js
@@ -2,8 +2,7 @@
 // ("Priyanshu");
 // false;
 
-// Arithmetic Operators *****
-
+// Arithmetic Operators
 console.log("Operators in Js");
 let a = 10;
 let b = 4;
@@ -18,12 +17,12 @@ console.log("a++ =", a++);
 console.log("--a =", --a);
 console.log("a-- =", a--);
 
-// Assignment Operators *****
+// Assignment Operators
 let assignment = 5;
 assignment += 5; // same as assignment = assignment + 5
 console.log(assignment);
 
-// Comparison Operators *****
+// Comparison Operators
 let comparison1 = 6;
 let comparison2 = 7;
 console.log("comparison1 == comparison2 is ", comparison1 == comparison2);
@@ -32,7 +31,7 @@ console.log("comparison1 === comparison2 is ", comparison1 === comparison2);
 console.log("comparison1 !== comparison2 is ", comparison1 !== comparison2);
 console.log("comparison1 > comparison2 is ", comparison1 > comparison2);
 
-// Logical Operators *****
+// Logical Operators
 let x = 5
 let y = 6
 console.log(x<y && x==5);
