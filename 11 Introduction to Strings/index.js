4b86b80d3e1d1f5a4954a983e7759d7c96845f47
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-29 10:31:49 +0530

    string

--- 11 Introduction to Strings/index.js
+++ 11 Introduction to Strings/index.js
@@ -1,23 +1,20 @@
-// Introduction to Strings *****
-
+// Strings
 let myname = "Priyanshu";
-// console.log(myname);
-// console.log(myname.length);
-// console.log(myname[0]);
-// console.log(myname[1]);
+console.log(myname);
+console.log(myname.length);
+console.log(myname[0]);
+console.log(myname[1]);
 
 let friend = "Adarsh";
-// console.log(friend);
+console.log(friend);
 
-// Template literals *****
-
+// Template literals
 let boy1 = "Promod";
 let boy2 = "Nikhil";
 console.log(`${boy2} is a friend of ${boy1}`);
 
-// Escape Sequence Characters *****
-
-// let fruit = 'Bana\'na';
-// console.log(fruit);
-let fruit = 'Bana"na';
+// Escape Sequence Characters
+let fruit = 'Bana\'na';
+console.log(fruit);
+// let fruit = 'Bana"na';
 console.log(fruit);
