d5a2fb12c6adc2a05557b71a1d21ce9185337c81
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-29 10:46:11 +0530

    exercise

---
+++ 27 Exercise 2 - Snake, Water and Gun/index.js
@@ -0,0 +1,3 @@
+// Exercise 2 - Snake, Water and Gun
+
+// Use JavaScript to create a game of Snake, Water & Gun. The game should ask you to enter 5, W or G. The computer should be able to randomly generate 5, W or G and declare win or loss using alert use confirm and prompt wherever required.
