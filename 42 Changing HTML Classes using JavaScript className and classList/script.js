96f1717ef8f3a5d0f0741b03dc3792bc9b9209ec
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-30 21:37:57 +0530

    Changing HTML Classes using JavaScript

---
+++ 42 Changing HTML Classes using JavaScript className and classList/script.js
@@ -0,0 +1,8 @@
+let first = document.querySelector("#first");
+
+first.className = "red text-dark";
+
+first.classList.add("red");
+first.classList.remove("red");
+first.classList.toggle("red");
+first.classList.contains("red");
