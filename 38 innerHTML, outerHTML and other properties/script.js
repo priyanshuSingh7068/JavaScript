13b0fec6e208ce0346bb00f01b284d0c48faef3c
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-30 14:21:43 +0530

    innerHTML, outerHTML and other properties

---
+++ 38 innerHTML, outerHTML and other properties/script.js
@@ -0,0 +1,18 @@
+let x = document.getElementsByTagName("span")[0];
+console.log(x);
+let y = document.getElementsByTagName("span")[0];
+console.dir(y);
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+
+console.log(first.innerHTML);
+console.log(first.outerHTML);
+
+// console.log(first.innerHTML)
+// {/* <b>I am span</b> */}
+// console.log(first.outerHTML)
+// {/* <span id="first"><b>I am span</b></span> */}
+
+// console.log(document.body.firstChild.data)
+console.log(document.body.textContent);
+// first.hidden = false
